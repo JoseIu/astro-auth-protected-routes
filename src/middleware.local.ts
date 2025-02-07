61a7f2942acbe4fc95a6b1d1d47d49ccd7c642cd
Joselu <joseluissgz97@gmail.com>
2025-02-07 23:13:10 +0100

    feat: add astro middleware

---
+++ src/middleware.local.ts
@@ -0,0 +1,36 @@
+//ESTE MIDDLEWARE NO FUNCIONA POR EL NOMBRE, ES SOLO PARA LOCAL, BORRAR .local PARA QUE FUNCIONE
+import type { MiddlewareNext } from 'astro';
+import { defineMiddleware } from 'astro:middleware';
+
+const privateRoutes = ['/profile'];
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const { url, request } = context;
+
+  const authHeaders = request.headers.get('authorization') ?? '';
+
+  if (privateRoutes.includes(url.pathname)) {
+    return checkLoaclAuth(authHeaders, next);
+  }
+
+  return next();
+});
+
+const checkLoaclAuth = (authHeaders: string, next: MiddlewareNext) => {
+  if (authHeaders) {
+    const authValue = authHeaders.split(' ').at(-1) ?? 'user:pass';
+    const decodedValue = atob(authValue).split(':');
+    const [username, password] = decodedValue;
+
+    if (username === 'admin' && password === 'admin') {
+      return next();
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"'
+    }
+  });
+};
